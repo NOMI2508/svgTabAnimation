53caaac7f0ebdd71ef6441388b307124a5f6a9d4
naveedqadir8149 <naveedqadri8149@gmail.com>
2022-05-23 15:49:37 +0500

    1

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component ,  OnInit, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
+  selectTab: any;
+  @ViewChild('tabs') tabs: IonTabs;
   constructor() {}
+  setCurrentTab(event) {
+    console.log(event);    
+    this.selectTab = this.tabs.getSelected();
+    console.log("selected tab",this.selectTab);
+  }
 
+
+  getIcon(){
+    switch (true) {
+      case this.selectTab=='tab1':return 'home';
+      case this.selectTab=='tab2':return 'search';
+      case this.selectTab=='tab3':return 'film';
+       case this.selectTab=='tab4':return 'heart';
+      case this.selectTab=='tab5':return 'person';
+        
+       
+    }
+  }
 }
